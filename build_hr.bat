f90cc5c77a4b76f5248e05626fc90e7b603e3691
Olivier <olivierpachoud@gmail.com>
2023-07-19 16:08:21 +0200

    better compile time

--- build_hr.bat
+++ build_hr.bat
@@ -1,13 +1,7 @@
 mkdir bin
+mkdir bin\obj
 
-clang++ src/Engine.cpp src/Game.cpp src/Image.cpp src/Input.cpp src/Timer.cpp src/Audio.cpp src/Tile.cpp src/Grid.cpp src/Color.cpp src/Random.cpp src/UnitManager.cpp src/Graphics.cpp ^
-    -o bin/Game.dll -shared -g ^
-    -std=c++20 ^
-    -Wall -Wextra -Wno-c99-designator -Wno-reorder-init-list -Wno-microsoft-enum-forward-reference ^
-    -I include/ ^
-    -I libs/include/ ^
-    -D BAT_RUN ^
-    -D HOT_RELOAD
+call .\hot_reload.bat
 
 clang++ src/Engine.cpp src/Input.cpp src/Image.cpp src/Timer.cpp bin/Game.lib ^
     -o bin/game.exe ^
