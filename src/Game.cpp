168f2c69d755222db60fc5fb3596529201c61c63
Tryliom <alexishaldy@gmail.com>
2023-07-22 23:41:46 +0200

    Optimize black border to side of the grid

--- src/Game.cpp
+++ src/Game.cpp
@@ -110,7 +110,12 @@ void OnFrame(FrameData *frameData, TimerData *timerData, const simgui_frame_desc
 
 	UpdateCamera();
 
-	Graphics::DrawRect({-(gridWidth / 2 + 10), -(gridHeight / 2 + 10)}, Vector2F(gridWidth + 20, gridHeight + 20), {0.2f, 0.2f, 0.2f, 0.8f});
+	// Draw a black rect to each side of the grid
+	Graphics::DrawRect({-(gridWidth / 2 + 10), -(gridHeight / 2 + 10)}, Vector2F(gridWidth + 20, 10), {0.2f, 0.2f, 0.2f, 1});
+	Graphics::DrawRect({-(gridWidth / 2 + 10), -(gridHeight / 2)}, Vector2F(10, gridHeight), {0.2f, 0.2f, 0.2f, 1});
+	Graphics::DrawRect({-(gridWidth / 2 + 10), gridHeight / 2}, Vector2F(gridWidth + 20, 10), {0.2f, 0.2f, 0.2f, 1});
+	Graphics::DrawRect({gridWidth / 2, -(gridHeight / 2)}, Vector2F(10, gridHeight), {0.2f, 0.2f, 0.2f, 1});
+
 	gameState->Grid.Update();
 
 	gameState->UnitManager.UpdateUnits();
